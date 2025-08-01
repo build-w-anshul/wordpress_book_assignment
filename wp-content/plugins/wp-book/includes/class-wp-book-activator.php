f55fcb51c995b4d24f5a618135ae78f807ba6737
Anshul <anshul.soni@hbwsl.com>
2025-08-01 12:04:26 +0530

    Initial wp_book setup using Local + wppb

---
+++ wp-content/plugins/wp-book/includes/class-wp-book-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/build-w-anshul/
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ * @author     Anshul <anshul.soni@hbwsl.com>
+ */
+class Wp_Book_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
